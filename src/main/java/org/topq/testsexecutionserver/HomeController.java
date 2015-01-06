b831862f0c548db06e3979c3e4b5fbb1f2026798
unknown <nimrod_t@nimrod-W8LT.ramdor.co.il>
2015-01-06 14:42:23 +0200

    remove commented out code

--- src/main/java/org/topq/testsexecutionserver/HomeController.java
+++ src/main/java/org/topq/testsexecutionserver/HomeController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-
 /**
  * Handles requests for the application home page.
  */
@@ -14,10 +13,8 @@ public class HomeController {
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public ModelAndView getPages() {
-
 		ModelAndView model = new ModelAndView("home");
 		return model;
-
 	}
-	
+
 }
